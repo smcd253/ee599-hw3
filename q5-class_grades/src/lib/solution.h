9c63018a5065d198a110bc1df5e648f22cd36e32
Spencer McDonough <shmcdono@usc.edu>
2020-02-07 14:44:36 -0800

    start q5, mac to linux

---
+++ q5-class_grades/src/lib/solution.h
@@ -0,0 +1,27 @@
+#ifndef TEMPLATE_SOLUTION_H
+#define TEMPLATE_SOLUTION_H
+
+#include <string>
+#include <vector>
+#include <map>
+
+struct Grades
+{
+  int math;
+  int computers;
+  int physics;
+  Grades() : math(0), computers(0), physics(0) {}
+  Grades(int x, int y, int z) : math(x), computers(y), physics(z) {}
+}
+class AcademicRecord
+{
+  private:
+    Grades* grades;
+  public:
+    // constructors
+    AcademicRecord
+    
+
+}
+
+#endif
